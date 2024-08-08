aa786b77df628ebcd33d74fdcd080c98f6a3de31
Spidy205 <70950547+Spidy205@users.noreply.github.com>
2024-08-08 19:30:15 +0545

    Delete down/index.php

--- down/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : '';
-if (!in_array($protocol, array('HTTP/1.1', 'HTTP/2', 'HTTP/2.0'), true)) {
-    $protocol = 'HTTP/1.0';
-}
-header("$protocol 503 Service Unavailable", true, 503);
-header('Content-Type: text/html; charset=utf-8');
-header('Retry-After: 30');
-?>
